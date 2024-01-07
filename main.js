8c0394042c1464933948559b0a6b6b2a9d4922e5
Takahiro Shizuki <takahiro_shizuki@yahoo.ne.jp>
2024-01-08 06:07:18 +0900

    fix: modify sources and layers.

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const map = new maplibregl.Map({
             },
             能登半島空中写真_輪島中: {
                 type: 'raster',
-                tiles: ['https://maps.gsi.go.jp/xyz/20240102noto_wazimanaka_0102do/{z}/{x}/{y}.png'],
+                tiles: ['https://maps.gsi.go.jp/xyz/20240102_noto_wazimanaka_0105do/{z}/{x}/{y}.png'],
                 attribution: '&copy; <a href="https://www.gsi.go.jp/BOUSAI/20240101_noto_earthquake.html?fbclid=IwAR2g3dvoK3GeS33I3X4F_W5REdGVFEWkLjOt3_cODQot5X4iUO_mmzxcFm8#2">地理院タイル 令和6年(2024年)能登半島地震に関する情報</a>',
             },
             能登半島空中写真_穴水: {
@@ -57,13 +57,6 @@ const map = new maplibregl.Map({
             maxzoom: 18
         },
         {
-            id: '能登半島空中写真_輪島東',
-            type: 'raster',
-            source: '能登半島空中写真_輪島東',
-            minzoom: 0,
-            maxzoom: 18
-        },
-        {
             id: '能登半島空中写真_珠洲_20240102',
             type: 'raster',
             source: '能登半島空中写真_珠洲_20240102',
@@ -76,7 +69,35 @@ const map = new maplibregl.Map({
             source: '能登半島空中写真_珠洲_20240105',
             minzoom: 0,
             maxzoom: 18
-        }
+        },
+            {
+                id: '能登半島空中写真_輪島中',
+                type: 'raster',
+                source: '能登半島空中写真_輪島中',
+                minzoom: 0,
+                maxzoom: 18
+            },
+            {
+                id: '能登半島空中写真_輪島東',
+                type: 'raster',
+                source: '能登半島空中写真_輪島東',
+                minzoom: 0,
+                maxzoom: 18
+            },
+            {
+                id: '能登半島空中写真_穴水',
+                type: 'raster',
+                source: '能登半島空中写真_穴水',
+                minzoom: 0,
+                maxzoom: 18
+            },
+            {
+                id: '能登半島空中写真_七尾',
+                type: 'raster',
+                source: '能登半島空中写真_七尾',
+                minzoom: 0,
+                maxzoom: 18
+            },
         ],
     },
 });
