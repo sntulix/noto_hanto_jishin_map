3fd324c5c9cbfbb339dfc23a966f6ef09ed471b8
Takahiro Shizuki <takahiro_shizuki@yahoo.ne.jp>
2024-01-08 05:39:03 +0900

    update: add a source of 穴水地区.

--- main.js
+++ main.js
@@ -31,6 +31,11 @@ const map = new maplibregl.Map({
                 type: 'raster',
                 tiles: ['https://maps.gsi.go.jp/xyz/20240102noto_wazimanaka_0102do/{z}/{x}/{y}.png'],
                 attribution: '&copy; <a href="https://www.gsi.go.jp/BOUSAI/20240101_noto_earthquake.html?fbclid=IwAR2g3dvoK3GeS33I3X4F_W5REdGVFEWkLjOt3_cODQot5X4iUO_mmzxcFm8#2">地理院タイル 令和6年(2024年)能登半島地震に関する情報</a>',
+            },
+            能登半島空中写真_穴水: {
+                type: 'raster',
+                tiles: ['https://maps.gsi.go.jp/xyz/20240102_noto_anamizu_0105do/{z}/{x}/{y}.png'],
+                attribution: '&copy; <a href="https://www.gsi.go.jp/BOUSAI/20240101_noto_earthquake.html?fbclid=IwAR2g3dvoK3GeS33I3X4F_W5REdGVFEWkLjOt3_cODQot5X4iUO_mmzxcFm8#2">地理院タイル 令和6年(2024年)能登半島地震に関する情報</a>',
             }
         },
         layers: [{
@@ -58,6 +63,14 @@ const map = new maplibregl.Map({
             id: '能登半島空中写真_輪島中',
             type: 'raster',
             source: '能登半島空中写真_輪島中',
+            paint: {
+                'raster-opacity': 0.9
+            }
+        },
+        {
+            id: '能登半島空中写真_穴水',
+            type: 'raster',
+            source: '能登半島空中写真_穴水',
             paint: {
                 'raster-opacity': 0.9
             }
