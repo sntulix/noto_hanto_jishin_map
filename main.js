6da7b09b03b9576c896427f097eaee8396650008
Takahiro Shizuki <takahiro_shizuki@yahoo.ne.jp>
2024-01-08 04:28:09 +0900

    update: add 3D buildings with maptiler.

--- main.js
+++ main.js
@@ -102,4 +102,51 @@ map.on('load', () => {
             exaggreation: 1
         }),
     );
+
+    const layers = map.getStyle().layers;
+
+    let labelLayerId;
+    for (let i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
+    }
+
+    map.addSource('openmaptiles', {
+        url: `https://api.maptiler.com/tiles/v3/tiles.json?key=Um12UgMHb9CkdMpzMrp9`,
+        type: 'vector',
+    });
+    map.addLayer(
+        {
+            'id': '3d-buildings',
+            'source': 'openmaptiles',
+            'source-layer': 'building',
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'render_height'], 0, 'lightgray', 200, 'royalblue', 400, 'lightblue'
+                ],
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    16,
+                    ['get', 'render_height']
+                ],
+                'fill-extrusion-base': ['case',
+                    ['>=', ['get', 'zoom'], 16],
+                    ['get', 'render_min_height'], 0
+                ],
+                'fill-extrusion-opacity': 0.4
+            }
+        },
+        labelLayerId
+    );
+
 });
