25d9cf9d70c12733172ef19948e54be65438a28a
Takahiro Shizuki <takahiro_shizuki@yahoo.ne.jp>
2024-01-17 00:54:45 +0900

    update: the codes of terrain and building to map_onload.js.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { useGsiTerrainSource } from 'maplibre-gl-gsi-terrain';
+import map_onload from './map_onload';
 
 /*
  * 穴水地区 空中写真(2024/01/11) を透過処理するためのプロトコル
@@ -173,66 +174,5 @@ const map = new maplibregl.Map({
 map.addControl(new maplibregl.NavigationControl());
 
 map.on('load', () => {
-    /* add Terrain */
-    const gsiTerrainSource = useGsiTerrainSource(maplibregl.addProtocol);
-    map.addSource('terrain', gsiTerrainSource);
-    map.addControl(
-        new maplibregl.TerrainControl({
-            source: 'terrain',
-            exaggreation: 1
-        }),
-    );
-
-    const layers = map.getStyle().layers;
-    let labelLayerId;
-    for (let i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-            labelLayerId = layers[i].id;
-            break;
-        }
-    }
-
-    /* add 3D-Buildings */
-    map.addSource('openmaptiles', {
-        url: `https://api.maptiler.com/tiles/v3/tiles.json?key=QvF1NsfIoRW0AGE6u8j7`,
-        type: 'vector',
-    });
-    map.addLayer(
-        {
-            'id': '3d-buildings',
-            'source': 'openmaptiles',
-            'source-layer': 'building',
-            'type': 'fill-extrusion',
-            'minzoom': 8,
-            'maxzoom': 17,
-            'paint': {
-                'fill-extrusion-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['get', 'render_height'], 0, 'yellow'
-                ],
-                'fill-extrusion-height': [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    8, 0,
-                    9, 0,
-                    10, 0,
-                    11, 0,
-                    12, 0,
-                    13, 0,
-                    14, 0,
-                    15, 0,
-                    16, ['get', 'render_height']
-                ],
-                'fill-extrusion-base': ['case',
-                    ['>=', ['get', 'zoom'], 16],
-                    ['get', 'render_min_height'], 0
-                ],
-                'fill-extrusion-opacity': 1.0
-            }
-        },
-        labelLayerId
-    );
-
+    map_onload(maplibregl, useGsiTerrainSource, map);
 });
---
+++ map_onload.js
@@ -0,0 +1,64 @@
+export default function map_onload(maplibregl, useGsiTerrainSource, map) {
+    /* add Terrain */
+    const gsiTerrainSource = useGsiTerrainSource(maplibregl.addProtocol);
+    map.addSource('terrain', gsiTerrainSource);
+    map.addControl(
+        new maplibregl.TerrainControl({
+            source: 'terrain',
+            exaggreation: 1
+        }),
+    );
+
+    const layers = map.getStyle().layers;
+    let labelLayerId;
+    for (let i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
+    }
+
+
+    /* add 3D-Buildings */
+    map.addSource('openmaptiles', {
+        url: `https://api.maptiler.com/tiles/v3/tiles.json?key=EAgHbAOM5T4rbNX721Sa`,
+        type: 'vector',
+    });
+    map.addLayer(
+        {
+            'id': '3d-buildings',
+            'source': 'openmaptiles',
+            'source-layer': 'building',
+            'type': 'fill-extrusion',
+            'minzoom': 8,
+            'maxzoom': 17,
+            'paint': {
+                'fill-extrusion-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'render_height'], 0, 'yellow'
+                ],
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    8, 0,
+                    9, 0,
+                    10, 0,
+                    11, 0,
+                    12, 0,
+                    13, 0,
+                    14, 0,
+                    15, 0,
+                    16, ['get', 'render_height']
+                ],
+                'fill-extrusion-base': ['case',
+                    ['>=', ['get', 'zoom'], 16],
+                    ['get', 'render_min_height'], 0
+                ],
+                'fill-extrusion-opacity': 1.0
+            }
+        },
+        labelLayerId
+    );
+}
