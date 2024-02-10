e8ff97e06122d654c8d2f0e512659c3709382b93
Takahiro Shizuki <takahiro_shizuki@yahoo.ne.jp>
2024-02-11 00:54:45 +0900

    update: add to the flight schedule of noto airport.

--- map_onload.js
+++ map_onload.js
@@ -64,7 +64,18 @@ export default function map_onload(maplibregl, useGsiTerrainSource, map) {
 
 
     /* add noto airport */
-    const marker = new maplibregl.Marker()
+    const el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage =
+            `url(https://www.ana.co.jp/guide/website/o-banner/offibana/images/asw01_s_sample.gif)`;
+        el.style.width = `88px`;
+        el.style.height = `31px`;
+
+        el.addEventListener('click', () => {
+            window.alert(marker.properties.message);
+        });
+        
+        const marker = new maplibregl.Marker({element: el})
         .setLngLat([136.958982305556, 37.2927485833333])
         .setPopup(new maplibregl.Popup().setHTML("<div><a href='https://www.ana.co.jp/ja/jp/special-notice/000873.html?p1=domestic&p2=ja&p3=global' rel='noopener noreferrer' target='_blank'>令和6年能登半島地震に伴う運休および臨時便の設定について(2月7日更新） - ANA</a></div>"))
         .addTo(map);
